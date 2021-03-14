6a2e3a768458d021a194eb644b6637174711274d
sunpeng <peng1365451772@163.com>
2021-03-14 15:37:26 +0800

    上传项目

---
+++ crowdfunding07-member-parent/crowdfunding10-member-mysql-provider/src/main/java/com/peng/crowd/service/impl/OrderServiceImpl.java
@@ -0,0 +1,72 @@
+package com.peng.crowd.service.impl;
+
+
+import com.peng.crowd.entity.po.AddressPO;
+import com.peng.crowd.entity.po.AddressPOExample;
+import com.peng.crowd.entity.vo.AddressVO;
+import com.peng.crowd.entity.vo.OrderProjectVO;
+import com.peng.crowd.mapper.AddressPOMapper;
+import com.peng.crowd.mapper.OrderPOMapper;
+import com.peng.crowd.mapper.OrderProjectPOMapper;
+import com.peng.crowd.service.api.OrderService;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class OrderServiceImpl implements OrderService {
+	
+	@Autowired
+	private OrderProjectPOMapper orderProjectPOMapper;
+	
+	@Autowired
+	private OrderPOMapper orderPOMapper;
+	
+	@Autowired
+	private AddressPOMapper addressPOMapper;
+
+	@Override
+	public OrderProjectVO getOrderProjectVO(Integer projectId, Integer returnId) {
+		
+		return orderProjectPOMapper.selectOrderProjectVO(returnId);
+	}
+
+	@Override
+	public List<AddressVO> getAddressVOList(Integer memberId) {
+		
+		AddressPOExample example = new AddressPOExample();
+		
+		example.createCriteria().andMemberIdEqualTo(memberId);
+		
+		List<AddressPO> addressPOList = addressPOMapper.selectByExample(example);
+		
+		List<AddressVO> addressVOList = new ArrayList<AddressVO>();
+		
+		for (AddressPO addressPO : addressPOList) {
+			AddressVO addressVO = new AddressVO();
+			BeanUtils.copyProperties(addressPO, addressVO);
+			
+			addressVOList.add(addressVO);
+		}
+		
+		return addressVOList;
+	}
+	
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRES_NEW, rollbackFor = Exception.class)
+	@Override
+	public void saveAddress(AddressVO addressVO) {
+		
+		AddressPO addressPO = new AddressPO();
+		
+		BeanUtils.copyProperties(addressVO, addressPO);
+		
+		addressPOMapper.insert(addressPO);
+		
+	}
+
+}
--- crowdfunding07-member-parent/crowdfunding17-member-api/src/main/java/com/peng/crowd/api/MySQLRemoteService.java
+++ crowdfunding07-member-parent/crowdfunding17-member-api/src/main/java/com/peng/crowd/api/MySQLRemoteService.java
@@ -1,28 +1,49 @@
 package com.peng.crowd.api;
 
 import com.peng.crowd.entity.po.MemberPO;
+import com.peng.crowd.entity.vo.AddressVO;
+import com.peng.crowd.entity.vo.DetailProjectVO;
+import com.peng.crowd.entity.vo.OrderProjectVO;
+import com.peng.crowd.entity.vo.PortalTypeVO;
+import com.peng.crowd.entity.vo.ProjectVO;
 import com.peng.crowd.util.ResultEntity;
+import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 @FeignClient("peng-crowd-mysql")
 public interface MySQLRemoteService {
 	
 	@RequestMapping("/get/memberpo/by/login/acct/remote")
-	ResultEntity<MemberPO> getMemberPOByLoginAcctRemote(@RequestParam("loginacct") String loginacct);
+	ResultEntity<MemberPO> getMemberPOByLoginAcctRemote(
+      @RequestParam("loginacct") String loginacct);
 
 	@RequestMapping("/save/member/remote")
-	ResultEntity<String> saveMember(@RequestBody MemberPO memberPO);
+	public ResultEntity<String> saveMember(@RequestBody MemberPO memberPO);
 
 	@RequestMapping("/save/project/vo/remote")
-	ResultEntity<String> saveProjectVORemote(@RequestBody ProjectVO projectVO, @RequestParam("memberId") Integer memberId);
-
-
+	ResultEntity<String> saveProjectVORemote(
+      @RequestBody ProjectVO projectVO,
+      @RequestParam("memberId") Integer memberId);
+	
 	@RequestMapping("/get/portal/type/project/data/remote")
-  ResultEntity<List<PortalTypeVO>> getPortalTypeProjectDataRemote();
-
+	public ResultEntity<List<PortalTypeVO>> getPortalTypeProjectDataRemote();
+	
 	@RequestMapping("/get/project/detail/remote/{projectId}")
-	ResultEntity<DetailProjectVO> getDetailProjectVORemote(@PathVariable("projectId") Integer projectId);
+	public ResultEntity<DetailProjectVO> getDetailProjectVORemote(
+      @PathVariable("projectId") Integer projectId);
+
+	@RequestMapping("/get/order/project/vo/remote")
+	ResultEntity<OrderProjectVO> getOrderProjectVORemote(@RequestParam("projectId") Integer projectId,
+      @RequestParam("returnId") Integer returnId);
+
+	@RequestMapping("/get/address/vo/remote")
+	ResultEntity<List<AddressVO>> getAddressVORemote(@RequestParam("memberId") Integer memberId);
+
+	@RequestMapping("/save/address/remote")
+	ResultEntity<String> saveAddressRemote(@RequestBody AddressVO addressVO);
 }
